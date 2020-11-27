d80f5176ea90845257690eccbd9184892ec59de7
Yannick Stachelscheid <yannickstach@googlemail.com>
2020-11-27 10:43:31 +0100

    lint code

--- src/__test__/checkers/merge.test.ts
+++ src/__test__/checkers/merge.test.ts
@@ -3,47 +3,47 @@ import { And, isCheckValid, Merge } from "../../core"
 import { EMail } from "../../validators"
 
 const checkA = Keys({
-    name: TypeString,
-    age: TypeNumber,
-    meta: Keys({
-        acceptedNewsletter: TypeBoolean
-    }),
+	name: TypeString,
+	age: TypeNumber,
+	meta: Keys({
+		acceptedNewsletter: TypeBoolean,
+	}),
 })
 
 const checkB = Keys({
-    phonenumber: TypeString,
-    email: And(TypeString, EMail)
+	phonenumber: TypeString,
+	email: And(TypeString, EMail),
 })
 
 test("value is valid, merged result is returned", () => {
-    const value = {
-        name: "Franz",
-        age: 42,
-        meta: {
-            acceptedNewsletter: true,
-        },
-        email: "franz@bavaria.one",
-        phonenumber: "+4913374242"
-    }
+	const value = {
+		name: "Franz",
+		age: 42,
+		meta: {
+			acceptedNewsletter: true,
+		},
+		email: "franz@bavaria.one",
+		phonenumber: "+4913374242",
+	}
 
-    const result = Merge(checkA, checkB)(value)
+	const result = Merge(checkA, checkB)(value)
 
-    expect(isCheckValid(result)).toBeTrue()
-    expect(result[1]).toEqual(value)
+	expect(isCheckValid(result)).toBeTrue()
+	expect(result[1]).toEqual(value)
 })
 
 test("value is invalid, error returned", () => {
-    const value = {
-        name: "Franz",
-        age: 42,
-        meta: {
-            acceptedNewsletter: true,
-        },
-        email: "franz@bavaria",
-        phonenumber: "+4913374242"
-    }
+	const value = {
+		name: "Franz",
+		age: 42,
+		meta: {
+			acceptedNewsletter: true,
+		},
+		email: "franz@bavaria",
+		phonenumber: "+4913374242",
+	}
 
-    const result = Merge(checkA, checkB)(value)
+	const result = Merge(checkA, checkB)(value)
 
-    expect(isCheckValid(result)).toBeFalse()
+	expect(isCheckValid(result)).toBeFalse()
 })
